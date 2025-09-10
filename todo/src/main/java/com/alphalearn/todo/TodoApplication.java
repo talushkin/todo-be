74d95d5156b8d36f568ebdb3d7e38b2e83d2a069
talushkin <ipadtal@gmail.com>
2025-09-10 12:36:15 +0300

    spring

---
+++ todo/src/main/java/com/alphalearn/todo/TodoApplication.java
@@ -0,0 +1,39 @@
+package com.alphalearn.todo;
+
+import com.alphalearn.todo.repository.TodoRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoApplication implements CommandLineRunner {
+	@org.springframework.context.annotation.Bean
+	public org.springframework.web.servlet.config.annotation.WebMvcConfigurer corsConfigurer() {
+		return new org.springframework.web.servlet.config.annotation.WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(org.springframework.web.servlet.config.annotation.CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:3000")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+						.allowCredentials(true);
+			}
+		};
+	}
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Autowired
+	private TodoRepository todoRepository;
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("All Todos -> {}", todoRepository.fetchAllTodos());
+	}
+}
